b369e34d43f523ed84466601b9342e74afdfa1e3
amir <orgamiryazdani@gmail.com>
2023-09-04 11:25:05 +0330

    add location List and useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header/Header";
+import LocationList from "./components/LocationList/LocationList";
 
 function App() {
   return <div>
+    <Toaster />
     <Header />
+    <LocationList />
   </div>
 }
 
---
+++ src/components/LocationList/LocationList.jsx
@@ -0,0 +1,32 @@
+import useFetch from "../../hooks/useFetch"
+
+function LocationList() {
+    const { data, isLoading } = useFetch("http://localhost:5000/hotels", "")
+
+    if (isLoading) return <p>loading...</p>
+
+    return (
+        <div className="nearbyLocation">
+            <h2>Nearby Location</h2>
+            <div className="locationList">
+                {
+                    data.map((item) => {
+                        return <div key={item.id} className="locationItem">
+                            <img src={item.picture_url.url} alt={item.name} />
+                            <div className="locationItemDesc">
+                                <p className="location">{item.smart_location}</p>
+                                <p className="name">{item.name}</p>
+                                <p className="price">
+                                    €&nbsp;{item.price}&nbsp;
+                                    <span>night</span>
+                                </p>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default LocationList
